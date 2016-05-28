4dffe6d2aea7f382903611beb7615aa1f491e162
Eric Schmidt <eric@Erics-MacBook-Air.local>
2016-05-28 12:39:08 -0600

    add Procfile for heroku deployment

--- src/main/java/com/sharedmeals/WebApplication.java
+++ src/main/java/com/sharedmeals/WebApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebappApplication extends SpringBootServletInitializer {
+public class WebApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WebappApplication.class);
+		return application.sources(WebApplication.class);
 	}
 	public static void main(String[] args) {
-		SpringApplication.run(WebappApplication.class, args);
+		SpringApplication.run(WebApplication.class, args);
 	}
 }
