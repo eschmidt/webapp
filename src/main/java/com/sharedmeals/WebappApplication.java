2531b78768b34432ccbdb467e0e7cdd9070cb71c
Eric Schmidt <eric@Erics-MacBook-Air.local>
2016-05-28 00:00:03 -0600

    package spring boot as war file and allow it to run successfully in tomcat

--- src/main/java/com/sharedmeals/WebappApplication.java
+++ src/main/java/com/sharedmeals/WebappApplication.java
@@ -2,10 +2,16 @@ package com.sharedmeals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebappApplication {
+public class WebappApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebappApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
